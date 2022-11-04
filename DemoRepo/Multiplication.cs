2fc8b5516b4a25343533fa5597d1011878b81674
dericklopis <stanforever08@gmail.com>
2022-11-04 08:57:50 +0530

    Adding multiplication

---
+++ DemoRepo/Multiplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRepo
+{
+    internal class Multiplication
+    {
+         
+              //access-modifier return-type methodName
+        public static void Multiplying3Numbers(int firstNumb, int sendNumb, int thirdNumb)
+        {
+            int result = firstNumb*sendNumb*thirdNumb;
+            Console.WriteLine("multiplication of numbers:"+ result);
+        }
+    }
+}
---
+++ DemoRepo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiplication.Multiplying3Numbers(10, 20, 30);
+            Console.ReadLine();
+        }
+    }
+}
